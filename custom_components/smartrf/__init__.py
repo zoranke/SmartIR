b8b49029b1c8a0f60036a84e16557bf872dafde8
EricHour <24457241+zoranke@users.noreply.github.com>
2020-09-19 05:31:03 +0800

    Update __init__.py

--- custom_components/smartrf/__init__.py
+++ custom_components/smartrf/__init__.py
@@ -18,16 +18,16 @@ from homeassistant.helpers.typing import ConfigType
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = 'smartir'
+DOMAIN = 'smartrf'
 VERSION = '1.13.1'
 MANIFEST_URL = (
     "https://raw.githubusercontent.com/"
-    "smartHomeHub/SmartIR/{}/"
-    "custom_components/smartir/manifest.json")
+    "zoranke/SmartRF/{}/"
+    "custom_components/smartrf/manifest.json")
 REMOTE_BASE_URL = (
     "https://raw.githubusercontent.com/"
-    "smartHomeHub/SmartIR/{}/"
-    "custom_components/smartir/")
+    "zoranke/SmartRF/{}/"
+    "custom_components/smartrf/")
 COMPONENT_ABS_DIR = os.path.dirname(
     os.path.abspath(__file__))
 
@@ -43,7 +43,7 @@ CONFIG_SCHEMA = vol.Schema({
 }, extra=vol.ALLOW_EXTRA)
 
 async def async_setup(hass, config):
-    """Set up the SmartIR component."""
+    """Set up the SmartRF component."""
     conf = config.get(DOMAIN)
 
     if conf is None:
@@ -81,21 +81,21 @@ async def _update(hass, branch, do_update=False, notify_if_latest=True):
                         if notify_if_latest:
                             hass.components.persistent_notification.async_create(
                                 "You're already using the latest version!", 
-                                title='SmartIR')
+                                title='SmartRF')
                         return
 
                     if StrictVersion(current_ha_version) < StrictVersion(min_ha_version):
                         hass.components.persistent_notification.async_create(
                             "There is a new version of SmartIR integration, but it is **incompatible** "
-                            "with your system. Please first update Home Assistant.", title='SmartIR')
+                            "with your system. Please first update Home Assistant.", title='SmartRF')
                         return
 
                     if do_update is False:
                         hass.components.persistent_notification.async_create(
-                            "A new version of SmartIR integration is available ({}). "
-                            "Call the ``smartir.update_component`` service to update "
+                            "A new version of SmartRF integration is available ({}). "
+                            "Call the ``smartrf.update_component`` service to update "
                             "the integration. \n\n **Release notes:** \n{}"
-                            .format(last_version, release_notes), title='SmartIR')
+                            .format(last_version, release_notes), title='SmartRF')
                         return
 
                     # Begin update
@@ -114,12 +114,12 @@ async def _update(hass, branch, do_update=False, notify_if_latest=True):
 
                     if has_errors:
                         hass.components.persistent_notification.async_create(
-                            "There was an error updating one or more files of SmartIR. "
-                            "Please check the logs for more information.", title='SmartIR')
+                            "There was an error updating one or more files of SmartRF. "
+                            "Please check the logs for more information.", title='SmartRF')
                     else:
                         hass.components.persistent_notification.async_create(
                             "Successfully updated to {}. Please restart Home Assistant."
-                            .format(last_version), title='SmartIR')
+                            .format(last_version), title='SmartRF')
     except Exception:
        _LOGGER.error("An error occurred while checking for updates.")
 
